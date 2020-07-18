5ea9b5fd7d1b27b82570893f9b07c2d5c2fd8c1e
Sam <sam.thompson@biarri.com>
2020-07-18 11:55:12 +1000

    add rectifying radius A

--- geodesy.py
+++ geodesy.py
@@ -3,7 +3,33 @@ from constants import coordinate_set, a, _f, false_easting, false_northing, cent
 
 def geographic_to_grid():
     print('performing conversion using {}'.format(coordinate_set))
-    pass
+
+    # Step 1: Compute ellipsiodal constants
+
+    f = 1/_f            # flattening 
+    e2 = f * (2-f)      # e^2
+    n = f / (2-f)       # n 
+
+    # Step 2a: Compute powers of n 
+    n2 = n ** 2
+    n3 = n ** 3
+    n4 = n ** 4
+    n5 = n ** 5
+    n6 = n ** 6 
+    n7 = n ** 7
+    n8 = n ** 8
+
+    # Step 2b: Compute rectifying radius A 
+    A = a / (1 + n) * (
+        1 
+        + (1/4) * n2 
+        + (1/64) * n4
+        + (1/256) * n6 
+        + (25/ 16384) * n8
+    )
+
+    assert round(A, 7)  == 6367449.145771
+
 
 
 
