16fef9f837ec56dd7a449bc5cf17041f9f16d614
Sam <sam.thompson@biarri.com>
2020-07-18 19:08:01 +1000

    fix convergence

--- convergence.py
+++ convergence.py
@@ -1,10 +1,16 @@
 import math
 from utils import get_cm
 
+radians = math.radians
+sin = math.sin 
+tan = math.tan 
+degrees = math.degrees
+atan = math.atan
+
 def convergence(lat, lng):
     """
-    Compute the grid convergence in decimal degrees for
-    latitude and longitude coordinates in the MGA (Map Grid of Australia) zones.
+    gives the angle between the meridian
+    and the grid-line parallel to the u-axis
     # TODO: get_cm only returns values inside MGA zones
     Works for both GDA20 and GDA94.
     accepts: 
@@ -14,18 +20,14 @@ def convergence(lat, lng):
         dG: grid convergence in decical degrees
     """
 
-    lat, lng = float(lat), float(lng)
+    dLat, dLng = float(lat), float(lng)
 
-    assert -85 < lat < 85, "latitude value {} is outside feasible bounds".format(lat)
-    assert 108 < lng < 156, "longitude value {} is outside MGA zones".format(lng)
+    assert -85 < dLat < 85, "latitude value {} is outside feasible bounds".format(lat)
+    assert 108 < dLng < 156, "longitude value {} is outside MGA zones".format(lng)
 
-    dev = math.radians(lng - get_cm(lng))
-    latitude = math.radians(lat)
-    rG = math.atan(-math.sin(latitude) * math.tan(dev))
-    dG = math.degrees(rG)
+    dev = radians(dLng - get_cm(dLng))
+    rLat = radians(dLat)
+    rG = atan(-sin(rLat) * tan(dev))
+    dG = degrees(rG)
 
     return dG
-
-
-if __name__ == "__main__":
-    print(convergence(80, 155))
