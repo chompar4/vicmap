7f52c5b759ec1ba6d72f0107ed0c528d73f15b24
Sam <chompar4@gmail.com>
2020-10-15 09:46:58 +1100

    natural imports

---
+++ vicmap/__init__.py
@@ -0,0 +1,30 @@
+from .datums import AGD66, AGD84, GDA20, GDA94, WGS84
+from .ellipsoids import ANS, CLARKE, GRS67, GRS80, WGS84, reference_ellipsoids
+from .grids import MGA20, MGA94, MGRS, VICGRID, VICGRID94
+from .points import GeoPoint, MGAPoint, MGRSPoint, VICPoint
+from .projections import lambert_conformal_conic, utm
+
+__all__ = [
+    AGD66,
+    AGD84,
+    GDA20,
+    GDA94,
+    WGS84,
+    ANS,
+    CLARKE,
+    GRS67,
+    GRS80,
+    WGS84,
+    MGA20,
+    MGA94,
+    MGRS,
+    VICGRID,
+    VICGRID94,
+    GeoPoint,
+    MGAPoint,
+    MGRSPoint,
+    VICPoint,
+    reference_ellipsoids,
+    lambert_conformal_conic,
+    utm,
+]
